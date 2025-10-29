5f656be785c6f90a2c573aa2c9bf2c21bb0e03f5
v-art-ika22 <vartikaagnihotri9402@gmail.com>
2025-10-29 11:20:57 +0530

    do

--- Frontend-FB/JAVASCRIPT/Basics/script.js
+++ Frontend-FB/JAVASCRIPT/Basics/script.js
@@ -1,6 +1,23 @@
-console.log("hello");
-alert("welcome to stupid class");
+//EXPERIMENT-5
+//TASK-1
+/*alert("welcome to stupid class");
 const nam=prompt("what is your name");
 console.log(nam);
 alert("hello "+ nam+  " welcome to my page");
-document.write("<h1>hello, "+ nam +"</h1>");
+document.write("<h1>hello, "+ nam +"</h1>");*/
+
+//TASK-2
+//let age=prompt("what is your age");
+    /*if(age>=18){
+        alert("congratulations....you can vote");
+    }
+    else{
+        alert("sorry,you cannot vote");
+    }*/
+   //(age>=18)?alert("yes"):alert("no");
+
+let str="18";
+let convert=Number(str);
+console.log(convert);
+
+
