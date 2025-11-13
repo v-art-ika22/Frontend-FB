69635f8f81b4c1f9b661d11eb46a748110236aa8
v-art-ika22 <vartikaagnihotri9402@gmail.com>
2025-11-13 20:29:07 +0530

    do

---
+++ Frontend-FB/JAVASCRIPT/oops in javascript/constructor.js
@@ -0,0 +1,11 @@
+/*const Person = function(name,age){
+    this.firstName=name;
+    this.age=age;
+}
+Person.prototype.introduce=function(){
+    console.log("hi my name is ${this.firstName} and i am ${this.age} years old");
+}
+const person1 = new Person("vartika", 21);
+person1.introduce;
+console.log(person1);*/
+
